ee1eadaee3816f57d0f58b70faa034d92bc9fade
vflorin1995 <vflorin1995@gmail.com>
2022-09-14 11:55:43 +0300

    Add hamburger button functionality

---
+++ script.js
@@ -0,0 +1,24 @@
+// menu button
+
+const menuButton = document.querySelector('.hamburger');
+const closeButton = document.querySelector('.closeButton');
+const menuList = document.querySelector('.menuList');
+const homeButton = document.querySelector('.homeButton');
+
+menuButton.addEventListener('click', () => {
+  console.log(menuList);
+  menuList.classList.add('menuOpen');
+  homeButton.classList.remove('displayNone');
+});
+
+closeButton.addEventListener('click', () => {
+  menuList.classList.remove('menuOpen');
+  homeButton.classList.add('displayNone')
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 768) {
+    menuList.classList.remove('menuOpen');
+    homeButton.classList.add('displayNone')
+  }
+});
